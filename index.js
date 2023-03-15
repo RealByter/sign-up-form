ba71500085ced01b19c34a5b4cb52ec0101f23fb
RealByter <justavishay@gmail.com>
2023-03-15 11:58:45 +0200

    feat: finish the inputs

---
+++ index.js
@@ -0,0 +1,16 @@
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+
+const verifyPassword = e => {
+	console.log(e);
+	if (password.value === confirmPassword.value && password.value >= 4) {
+		password.classList.remove('invalid');
+		confirmPassword.classList.remove('invalid');
+	} else {
+		password.classList.add('invalid');
+		password.classList.add('invalid');
+	}
+};
+
+password.addEventListener('input', verifyPassword);
+confirmPassword.addEventListener('input', verifyPassword);
